8bb6a3001d05373748c873f56cbfcf5b3a2a8acf
jackbaber <thejackbabs@gmail.com>
2018-01-15 10:53:30 +0000

    added mouseover and mouseout functionality to the list items

--- events.js
+++ events.js
@@ -5,6 +5,8 @@ var inputs = checklist.querySelectorAll("input");
 
 for(var i = 0; i < items.length; i++) {
     items[i].addEventListener("click", editItem);
+    items[i].addEventListener("mouseover", addListItemStyle);
+    items[i].addEventListener("mouseout", removeListItemStyle);
     inputs[i].addEventListener("blur", updateItem);
     inputs[i].addEventListener("keypress", itemKeypress);
 }
@@ -27,35 +29,11 @@ function itemKeypress(event){
     }
 }
 
+function addListItemStyle(){
+    this.classList.add("emphasised");
+}
 
+function removeListItemStyle(){
+    this.classList.remove("emphasised");
+}
 
-
-
-// var checklist = document.getElementById("checklist");
-
-// var items = checklist.querySelectorAll("li");
-// var inputs = checklist.querySelectorAll("input");
-
-// for (var i = 0; i < items.length; i++) {
-//   items[i].addEventListener("click", editItem);
-//   inputs[i].addEventListener("blur", updateItem);
-//   inputs[i].addEventListener("keypress", itemKeypress);
-// }
-
-// function editItem() {
-//   this.className = "edit";
-//   var input = this.querySelector("input");
-//   input.focus();
-//   input.setSelectionRange(0, input.value.length);
-// }
-
-// function updateItem() {
-//   this.previousElementSibling.innerHTML = this.value;
-//   this.parentNode.className = "";
-// }
-
-// function itemKeypress(event) {
-//   if (event.which === 13) {
-//     updateItem.call(this);
-//   }
-// }
